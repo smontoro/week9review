6fc143fd218ed057e03fa8d2ddd2037631e511f6
smontoro <sarahjmontoro@gmail.com>
2018-07-18 13:50:03 -0700

    cleaned up

--- js/main.js
+++ js/main.js
@@ -2,22 +2,15 @@ var newEmpty = []
 var userInput = document.getElementById("userInput")
 
 function myFunk(){
-	//push the new value to the array
 	newEmpty.push(userInput.value)
 
 	var lastItem = newEmpty[newEmpty.length - 1]
-
-	console.log(lastItem)
-
 	var t = document.createTextNode(lastItem)
 	var li = document.createElement("li")
 	li.appendChild(t)
 	var ol = document.getElementsByTagName("ol")[0]
 	ol.appendChild(li)
 
-
-
 	//clear the input field
 	userInput.value = ' '
-
 }
