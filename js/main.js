bd4efb5f25837a497df232f52a37c66c641e2fab
smontoro <sarahjmontoro@gmail.com>
2018-07-18 15:48:19 -0700

    added buttons

--- js/main.js
+++ js/main.js
@@ -1,16 +1,43 @@
 var newEmpty = []
 var userInput = document.getElementById("userInput")
+var ul = document.getElementsByTagName("ul")[0]
+
+function appendFunk(){
+	ul.innerHTML = ""
+
+	for(i = 0; i < newEmpty.length; i++){
+		if(newEmpty[i] !== " ") {
+			var t = document.createTextNode(newEmpty[i])
+			var li = document.createElement("li")
+			li.appendChild(t)
+			ul.appendChild(li)
+		} 
+	}
+};
 
 function myFunk(){
+	if(userInput.value !== " "){
+		//push the input value into the array
 	newEmpty.push(userInput.value)
-
-	var lastItem = newEmpty[newEmpty.length - 1]
-	var t = document.createTextNode(lastItem)
-	var li = document.createElement("li")
-	li.appendChild(t)
-	var ol = document.getElementsByTagName("ol")[0]
-	ol.appendChild(li)
-
+	ul.innerHTML = ""
+	appendFunk()	
 	//clear the input field
 	userInput.value = ' '
+
+	}
+}
+
+function sortFunk(){
+	newEmpty.sort()
+	appendFunk()
+}
+
+function popFunk(){
+	newEmpty.pop()
+	appendFunk()
+}
+
+function shiftFunk(){
+	newEmpty.shift()
+	appendFunk()
 }
